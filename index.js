9de60175ffe96c96f741c6838d385c5b1f163b8f
Andrew Welsh <decoderandy@gmail.com>
2021-02-10 15:10:49 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+const hamburgerBtn = document.getElementById('hamburger');
+const navList = document.getElementById('nav-list');
+
+function toggleButton() {
+  navList.classList.toggle('show')
+}
+
+hamburgerBtn.addEventListener('click', toggleButton)
+
+function myFunction() {
+  var element = document.body;
+  element.classList.toggle();
+}
